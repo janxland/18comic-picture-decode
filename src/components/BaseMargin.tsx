d1b3e1e7d295fa4a0b9b5b7d8e407af91fa4f3d9
MiaoMint <1981324730@qq.com>
2023-03-30 17:09:40 +0800

    fix: 右边距异常

--- src/components/BaseMargin.tsx
+++ src/components/BaseMargin.tsx
@@ -5,7 +5,7 @@ export default function BaseMargin(props: HTMLProps<HTMLDivElement>) {
     return (
         <div
             {...props}
-            className={clsx("pl-5 pt-5 md:pl-10 md:pr-10 md:pt-10",props.className)} >
+            className={clsx("pl-5 pt-5 pr-5 md:pl-10 md:pr-10 md:pt-10",props.className)} >
             {props.children}
         </div>
     )
