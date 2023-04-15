43e07fe08d5d96c7820dccf389b84ff6f225a919
MiaoMint <1981324730@qq.com>
2023-04-15 23:05:09 +0800

    refactor: 去除多余的组件

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -1,9 +1,92 @@
 "use client"
-import WatchPage from "./watch-page";
+import BaseMargin from "@/components/BaseMargin";
+import ErrorView from "@/components/ErrorView";
+import { useRootStore } from "@/context/root-context";
+import { WatchData, WatchProvider } from "@/context/watch-context";
+import { useQuery } from "@tanstack/react-query";
+import { observer } from "mobx-react-lite";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useTranslation } from "../i18n";
+import { Background } from "./Background";
+import BaseDetail from "./BaseDetail";
+import Credits from "./Credits";
+import Episodes from "./Episodes";
+import Footer from "./Footer";
+import Player from "./Player";
+import Skeletion from "./Skeletion";
+;
 
+const WatchPage = observer(() => {
+    const searchParams = useSearchParams()
+    const route = useRouter()
+    const { extensionStore } = useRootStore()
+    const pkg = searchParams?.get("pkg") as string
+    const url = searchParams?.get("url") as string
+    const cover = searchParams?.get("cover") as string
+    const extension = extensionStore.getExtension(pkg)
+    const { t } = useTranslation("watch")
+    const [watchData, setWatchData] = useState<WatchData>()
+    const { isLoading, error, isError, data } = useQuery({
+        queryKey: ["watch", pkg, url],
+        queryFn: () => extension?.detail(url)
+    })
+    useEffect(() => {
+        if (data) {
+            setWatchData({
+                detail: data!,
+                pkg,
+                url,
+                extension: extension!,
+                setWatchData,
+            })
+        }
+    }, [data])
 
-export default function Page() {
+    if (!pkg || !url) {
+        // 跳转到404
+        route.push("/404")
+        return <></>
+    }
+    if (!extension) {
+        return (
+            <ErrorView error={t('extension-lost', { pkg })} ></ErrorView>
+        )
+    }
+
+    if (isError) {
+        return (
+            <ErrorView error={error} />
+        )
+    }
+
+    if (isLoading || !watchData) {
+        return (
+            <Skeletion cover={cover} />
+        )
+    }
+
+    if (!data) {
+        return (
+            <ErrorView error={t('not-found-data')}></ErrorView>
+        )
+    }
+
     return (
-        <WatchPage />
+        <WatchProvider value={watchData} >
+            <Background />
+            <BaseMargin>
+                <div className="min-h-screen">
+                    <Player />
+                    <BaseDetail />
+                    <Episodes />
+                    <Credits />
+                </div>
+                <Footer />
+            </BaseMargin>
+        </ WatchProvider>
     )
-}
+})
+
+export default WatchPage
+
