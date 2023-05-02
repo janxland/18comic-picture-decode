859041d5e907865cec37eef98e9c583880bc2821
MiaoMint <1981324730@qq.com>
2023-05-03 01:34:10 +0800

    feat: 保持搜索的tab索引

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -24,11 +24,11 @@ const SearchPage = observer(() => {
     const [kw, setKW] = useState("");
     const [inputKW, setInputKW] = useState("");
 
-
     useEffect(() => {
         if (param) {
-            setKW(param.get("kw") as string);
-            setInputKW(param.get("kw") as string);
+            setKW(param.get("kw") ?? "");
+            setInputKW(param.get("kw") ?? "");
+            setTabIndex(parseInt(param.get("tab") ?? "0"));
         }
     }, [param]);
 
@@ -37,27 +37,29 @@ const SearchPage = observer(() => {
     }, []);
 
     useEffect(() => {
-        setTabIndex(0);
-    }, [settingsStore.getSetting("model")]);
-
-    useEffect(() => {
         const newTabs: Array<Tabs> = [];
-        const extensions = extensionStore
-            .getExtensionsByType(getModel(settingsStore.getSetting("model")));
+        const extensions = extensionStore.getExtensionsByType(
+            getModel(settingsStore.getSetting("model"))
+        );
 
         // 搜索全部扩展
-
         extensions.map((value: Extension) => {
             newTabs.push({
                 title: value.name,
-                content: <Result extension={value} kw={kw} />
+                content: <Result extension={value} kw={kw} />,
             });
         });
 
         if (newTabs.length > 1) {
             newTabs.unshift({
                 title: t("all"),
-                content: <SearchAll toTab={(index) => setTabIndex(index)} extensions={extensions} kw={kw} />
+                content: (
+                    <SearchAll
+                        toTab={(index) => handleSearch(index)}
+                        extensions={extensions}
+                        kw={kw}
+                    />
+                ),
             });
         }
         setTabs(newTabs);
@@ -65,13 +67,17 @@ const SearchPage = observer(() => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        route.replace(`/search?kw=${inputKW}`);
+        handleSearch(tabIndex);
+    };
+
+    const handleSearch = (index: number) => {
+        route.replace(`/search?kw=${inputKW}&tab=${index}`);
     };
 
     return (
         <Layout>
             <BaseMargin>
-                <SwitchTitle title={t("title")} />
+                <SwitchTitle handleTabChange={()=>handleSearch(0)} title={t("title")} />
                 <form className="mb-6" onSubmit={handleSubmit}>
                     <input
                         type="text"
@@ -83,15 +89,20 @@ const SearchPage = observer(() => {
                 </form>
 
                 {/*无扩展时显示的界面*/}
-                {tabs.length === 0 && (
+                {(tabs.length === 0 && (
                     <div className="mt-28 text-center">
                         <p className="text-2xl font-bold">
                             {t("no-extension")}
                         </p>
                         <p className="text-sm">{t("no-extension-tips")}</p>
                     </div>
-                ) || (
-                    <Tab className="mb-6" tabs={tabs} index={tabIndex} onChange={(index) => setTabIndex(index)}></Tab>
+                )) || (
+                    <Tab
+                        className="mb-6"
+                        tabs={tabs}
+                        index={tabIndex}
+                        onChange={handleSearch}
+                    ></Tab>
                 )}
             </BaseMargin>
         </Layout>
